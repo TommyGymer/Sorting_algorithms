a3b18ca6a360ab85d4ec158f24ec95dc856c66ac
TommyGymer <tommygymer@gmail.com>
2022-02-14 14:56:33 +0000

    Removing old code

--- main.py
+++ main.py
@@ -41,67 +41,14 @@ window = Tk()
 window.geometry(str(width) + "x" + str(height))
 #window.configure(background='blue')
 
-#img = Image.open("bean_r.png")
-#img = Image.open("garaph.jpg")
-#
-#img.show()
-
-#print(np.asarray(img))
-
 a = np.array(test.items)
-#b = np.ones(len(Sort.items))
-
-#pain = []
-
-#for i in range(width):
-#    paintemp = []
-#    for j in range(height):
-#        paintemp.append([a[int()], 1, 1])
-#    pain.append(paintemp)
-
-#print(np.array(pain))
-
-#print(np.vstack((a, b , b)).T)
-
-#print(np.block([np.flip(np.array(Sort.items), 1), np.ones((len(Sort.items),1)), np.ones((len(Sort.items),1))]))
-
-#panel = Label(window, image = ImageTk.PhotoImage(Image.fromarray(np.vstack((a, b , b)).T, mode="HSV")))
-#panel = Label(window, image = ImageTk.PhotoImage(img))
-
-#print(np.array((np.vstack((a, b , b)).T, np.vstack((a, b , b)).T)))
-#img = Image.fromarray(np.array((np.vstack((a, b , b)).T, np.vstack((a, b , b)).T)), mode="HSV").resize((width, height))
-#img = Image.fromarray(np.array(pain), mode="HSV")#.rotate(90)
-
-#pain = np.array([a]).T
-#pain = np.insert(pain, 1, 1, axis=1)
-#pain = np.insert(pain, 1, 1, axis=1)
 
 pain = [(int(255*a[int(steps*(x/width))]), 255, 255) for x in range(width)]
 asdf = [pain for i in range(height)]
 
-#print(np.array(asdf))
-
-#print(pain)
-#img = Image.new("HSV", (width, height))
-
-#for i in range(width):
-#    for j in range(height):
-#        img.putpixel((i, j), (int(255*(a[int((i * steps)/width)])), 255, 255))
-
-#print(np.array(a).T)
-#print(np.asarray(img))
-
-#print("---------------------")
-
-#img = Image.fromarray(, mode="HSV")
-
-#print(np.array(asdf))
-
 img = Image.fromarray(np.array(asdf, dtype=np.uint8), mode="HSV")
 
 nande = ImageTk.PhotoImage(img)
-
-#print(np.asarray(img))
 
 canvas = Canvas(window, bg='grey', width=width, height=height)
 canvas.pack(fill="both", expand="yes")
